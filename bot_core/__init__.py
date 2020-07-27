ced56031dcd0bf709095e573694b235a525745b9
Sithief <gojkljkl@gmail.com>
2020-07-27 23:58:29 +0700

    Update __init__.py

--- bot_core/__init__.py
+++ bot_core/__init__.py
@@ -13,6 +13,7 @@ base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 log_path = os.path.join(base_path, 'log')
 conf_path = os.path.join(base_path, 'config.cfg')
 db_path = os.path.join(base_path, 'data.db')
+CONF = dict()
 
 
 def init_logging():
@@ -33,22 +34,33 @@ def init_logging():
 def logging_excepthook(exctype, value, tb):
     import logging
     import time
-    log_path = init_logging()
+    init_logging()
     logging.critical(f'EXCEPTION: Type: {exctype}, Value: {value}')
     with open(os.path.join(log_path, 'bot_errors.log'), 'w') as error_file:
         error_file.write(time.asctime() + '\n')
         traceback.print_exception(exctype, value, tb, file=error_file)
 
 
-CONF = configparser.ConfigParser()
-CONF.read(conf_path, encoding='utf-8')
+def read_config():
+    global CONF
+    is_prod = os.environ.get('ENVIRON', None)
+    if is_prod:
+        CONF = {'VK_token': os.environ.get('VK_TOKEN', None),
+                'VK_confirm': os.environ.get('VK_CONFIRM', None)}
+    else:
+        conf = configparser.ConfigParser()
+        conf.read(conf_path, encoding='utf-8')
+        CONF = {'VK_token': conf.get('VK', 'token'),
+                'VK_confirm': conf.get('VK', 'confirm')}
 
+
+read_config()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
-bot_api = vk_api.Api(CONF.get('VK', 'token', fallback='no confirm'), 'main')
+bot_api = vk_api.Api(CONF.get('VK_token'), 'main')
 bot_app = Bot()
 
 init_logging()
